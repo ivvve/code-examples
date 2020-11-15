144aa6128eaaf5f71ce401b899b39b0bc39cc1ee
devson <sonyc5720@gmail.com>
2020-11-15 20:58:11 +0900

    tdd by example: added equals method

--- tdd-by-example/src/main/kotlin/money/Money.kt
+++ tdd-by-example/src/main/kotlin/money/Money.kt
@@ -4,4 +4,20 @@ class Dollar(var amount: Int) {
     fun times(n: Int): Dollar {
         return Dollar(this.amount * n)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Dollar
+
+        if (amount != other.amount) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return amount
+    }
+
 }
