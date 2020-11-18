82a3e77c82285b767f6206ba55d57cce260c5313
devson <sonyc5720@gmail.com>
2020-11-18 23:35:31 +0900

    tdd by example: 5-2 copied dollar for franc

--- tdd-by-example/src/main/kotlin/money/Money.kt
+++ tdd-by-example/src/main/kotlin/money/Money.kt
@@ -20,3 +20,24 @@ class Dollar(private val amount: Int) {
         return amount
     }
 }
+
+class Franc(private val amount: Int) {
+    fun times(multiplier: Int): Franc {
+        return Franc(this.amount * multiplier)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Franc
+
+        if (amount != other.amount) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return amount
+    }
+}
