0a4b0d374f64eb2d9421c04f31e53e34a9741812
devson <sonyc5720@gmail.com>
2020-11-18 23:49:51 +0900

    tdd by example: 6-4 changed Franc to inherit Money super class

--- tdd-by-example/src/main/kotlin/money/Money.kt
+++ tdd-by-example/src/main/kotlin/money/Money.kt
@@ -23,23 +23,8 @@ class Dollar(amount: Int): Money(amount) {
     }
 }
 
-class Franc(private val amount: Int) {
+class Franc(amount: Int): Money(amount) {
     fun times(multiplier: Int): Franc {
         return Franc(this.amount * multiplier)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Franc
-
-        if (amount != other.amount) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return amount
     }
 }
