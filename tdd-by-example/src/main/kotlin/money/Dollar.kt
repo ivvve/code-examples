add2776d2361c2d2682a051fa560c4ba72c18e91
devson <sonyc5720@gmail.com>
2020-11-18 23:25:06 +0900

    tdd by example: 3-2 dollar equals & hashcode method

--- tdd-by-example/src/main/kotlin/money/Dollar.kt
+++ tdd-by-example/src/main/kotlin/money/Dollar.kt
@@ -4,4 +4,19 @@ class Dollar(var amount: Int) {
     fun times(multiplier: Int): Dollar {
         return Dollar(this.amount * multiplier)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Dollar
+
+        if (amount != other.amount) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return amount
+    }
 }
