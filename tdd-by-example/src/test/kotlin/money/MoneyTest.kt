29b728bb77076042e2ffc2950a6ee7c44d8646dd
devson <sonyc5720@gmail.com>
2020-11-18 23:07:08 +0900

    tdd by example: 1-1 Dollar test code

--- tdd-by-example/src/test/kotlin/money/MoneyTest.kt
+++ tdd-by-example/src/test/kotlin/money/MoneyTest.kt
@@ -1,26 +1,14 @@
 package money
 
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class MoneyTest {
+
     @Test
     fun testMultiplication() {
-        val five = Dollar(5)
-        assertThat(five.times(2)).isEqualTo(Dollar(10))
-        assertThat(five.times(3)).isEqualTo(Dollar(15))
-    }
-
-    @Test
-    fun testEquality() {
-        assertThat(Dollar(5)).isEqualTo(Dollar(5))
-        assertThat(Dollar(5)).isNotEqualTo(Dollar(6))
-    }
-
-    @Test
-    fun testFracMultiplication() {
-        val five = Dollar(5)
-        assertThat(five.times(2)).isEqualTo(Dollar(10))
-        assertThat(five.times(3)).isEqualTo(Dollar(15))
+        val dollar = Dollar(5)
+        dollar.times(2)
+        assertThat(dollar.amount).isEqualTo(10)
     }
 }
