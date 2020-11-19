9d7523bead503b6a61e4322dffcea653dcdf06b7
devson <sonyc5720@gmail.com>
2020-11-19 10:17:02 +0900

    tdd by example: 12-1 added plus test

--- tdd-by-example/src/test/kotlin/money/MoneyTest.kt
+++ tdd-by-example/src/test/kotlin/money/MoneyTest.kt
@@ -31,4 +31,10 @@ class MoneyTest {
         assertThat("USD").isEqualTo(Money.dollar(1).currency())
         assertThat("CHF").isEqualTo(Money.franc(1).currency())
     }
+
+    @Test
+    fun testSimpleAddition() {
+        val sum = Money.dollar(5).plus(Money.dollar(5))
+        assertThat(sum).isEqualTo(Money.dollar(10))
+    }
 }
