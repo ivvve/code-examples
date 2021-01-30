60dfd6c11359c2e93a99c9dc425efc15b0f82376
devson <sonyc5720@gmail.com>
2021-01-30 16:14:50 +0900

    [security] change UserDetailsService to JdbcUserDetailsManager

--- spring-security-basics/build.gradle.kts
+++ spring-security-basics/build.gradle.kts
@@ -18,6 +18,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- spring-security-basics/src/main/kotlin/io/github/ivvve/security/config/WebSecurityConfig.kt
+++ spring-security-basics/src/main/kotlin/io/github/ivvve/security/config/WebSecurityConfig.kt
@@ -6,9 +6,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
+import javax.sql.DataSource
 
 @Configuration
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
@@ -32,15 +35,30 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 //                .and()
 //            .passwordEncoder(NoOpPasswordEncoder.getInstance()) // no encryption
 
-        val userDetailsService = InMemoryUserDetailsManager()
-
-        val admin = User.withUsername("admin").password("1234").authorities("ROLE_ADMIN").build()
-        val user = User.withUsername("user").password("1234").authorities("ROLE_USER").build()
-        userDetailsService.createUser(admin)
-        userDetailsService.createUser(user)
-
-        auth!!.userDetailsService(userDetailsService)
+//        val userDetailsService = InMemoryUserDetailsManager()
+//
+//        val admin = User.withUsername("admin").password("1234").authorities("ROLE_ADMIN").build()
+//        val user = User.withUsername("user").password("1234").authorities("ROLE_USER").build()
+//        userDetailsService.createUser(admin)
+//        userDetailsService.createUser(user)
+//
+//        auth!!.userDetailsService(userDetailsService)
         // `UserDetailsService`를 등록하고 `PasswordEncoder` bean을 등록하지 않으면 오류가 난다
+
+        super.configure(auth)
+    }
+
+    @Bean
+    fun userDetailsService(dataSource: DataSource): UserDetailsService {
+        /**
+         * 아래와 같은 query를 통해 UserDetails를 가져온다
+         * SELECT username, password, enabled FROM users
+         *
+         * String userName = rs.getString(1);
+         * String password = rs.getString(2);
+         * boolean enabled = rs.getBoolean(3);
+         */
+        return JdbcUserDetailsManager(dataSource)
     }
 
     @Bean
