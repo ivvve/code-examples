debd2a50e7f6d11b2a531845a3a8cf869f886f07
devson <sonyc5720@gmail.com>
2021-01-30 14:43:34 +0900

    [security] add in-memory authentication

--- spring-security-basics/src/main/kotlin/io/github/ivvve/security/config/WebSecurityConfig.kt
+++ spring-security-basics/src/main/kotlin/io/github/ivvve/security/config/WebSecurityConfig.kt
@@ -1,8 +1,10 @@
 package io.github.ivvve.security.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
 
 @Configuration
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
@@ -15,5 +17,15 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
             .formLogin()
                 .and()
             .httpBasic()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        // in-memory authentication 설정 시 spring.security.user 설정은 무시 된다
+        auth!!.inMemoryAuthentication()
+            .withUser("admin").password("1234").authorities("ROLE_ADMIN")
+                .and()
+            .withUser("user").password("1234").authorities("ROLE_USER")
+                .and()
+            .passwordEncoder(NoOpPasswordEncoder.getInstance()) // no encryption
     }
 }
