3632812619e7f001bd00a3a380a6148327416997
devson <sonyc5720@gmail.com>
2021-07-04 18:20:24 +0900

    [twooter-node] add `onTwoot` test

--- twooter-node/src/twooter/SenderEndpoint.ts
+++ twooter-node/src/twooter/SenderEndpoint.ts
@@ -1,6 +1,10 @@
-import { ReceiverEndPoint } from './ReceiverEndPoint';
 import { FollowStatus } from './FollowStatus';
 
 export interface SenderEndpoint {
+  /**
+   * 다른 유저를 Follow 할 때 사용되는 메서드
+   * @param targetUserId Follow 하고자하는 유저의 ID
+   * @return FollowStatus Follow 결과
+   */
   onFollow(targetUserId: String): FollowStatus;
 }
--- twooter-node/test/twooter/MockReceiverEndPoint.ts
+++ twooter-node/test/twooter/MockReceiverEndPoint.ts
@@ -1,5 +1,10 @@
 import { ReceiverEndPoint } from '../../src/twooter/ReceiverEndPoint';
+import { Twoot } from '../../src/twooter/Twoot';
 
 export class MockReceiverEndPoint implements ReceiverEndPoint {
+  readonly receivedTwoots: Array<Twoot> = [];
 
+  onTwoot(twoot: Twoot) {
+    this.receivedTwoots.push(twoot);
+  }
 }
