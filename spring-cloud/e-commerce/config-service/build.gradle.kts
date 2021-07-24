04677d2a3281d646b33f74d786c3a2d629583796
devson <sonyc5720@gmail.com>
2021-07-25 00:38:27 +0900

    [spring cloud] apply symmetric encryption

--- spring-cloud/e-commerce/config-service/build.gradle.kts
+++ spring-cloud/e-commerce/config-service/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-config-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
