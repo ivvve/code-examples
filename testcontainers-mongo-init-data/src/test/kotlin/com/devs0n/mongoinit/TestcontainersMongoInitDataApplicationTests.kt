a9bb53766d6d249bf591724d3c24ef0bb49e1baa
devson <sonyc5720@gmail.com>
2021-10-29 22:17:11 +0900

    [testcontainers-mongo-init-data] change example code

--- testcontainers-mongo-init-data/src/test/kotlin/com/devs0n/mongoinit/TestcontainersMongoInitDataApplicationTests.kt
+++ testcontainers-mongo-init-data/src/test/kotlin/com/devs0n/mongoinit/TestcontainersMongoInitDataApplicationTests.kt
@@ -1,13 +1,27 @@
 package com.devs0n.mongoinit
 
+import com.devs0n.mongoinit.member.MemberRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
 
-@SpringBootTest
-class TestcontainersMongoInitDataApplicationTests {
+class TestcontainersMongoInitDataApplicationTests : IntegrationTest() {
 
     @Test
     fun contextLoads() {
     }
 
+    @Autowired
+    lateinit var memberRepository: MemberRepository
+
+    @Test
+    fun `test init script`() {
+        val initMember = memberRepository.findById("chris")
+        assertThat(initMember).isNotNull
+
+        val member = initMember.get()
+        assertThat(member.id).isEqualTo("chris")
+        assertThat(member.name).isEqualTo("Chris")
+        assertThat(member.address).isEqualTo("Seoul")
+    }
 }
