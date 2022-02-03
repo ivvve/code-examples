4ff9df5f92675896ce9257d09147955720e4a331
devson <sonyc5720@gmail.com>
2022-02-03 12:46:37 +0900

    [kt-gof] memento

--- kt-gof/src/main/kotlin/com/tistory/devs0n/memento/lecture/Game.kt
+++ kt-gof/src/main/kotlin/com/tistory/devs0n/memento/lecture/Game.kt
@@ -1,10 +1,8 @@
 package com.tistory.devs0n.memento.lecture
 
 class Game {
-    var blueTeamScore = 0
-        private set
+    private var blueTeamScore = 0
     private var redTeamScore = 0
-        private set
 
     fun increaseBlueTeamScore(score: Int) {
         this.blueTeamScore += score
