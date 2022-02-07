983e291d30628a3b58757d9f1a632ed614f6e2a1
devson <sonyc5720@gmail.com>
2022-02-07 12:21:52 +0900

    [kt-gof] observer

--- kt-gof/src/main/kotlin/com/tistory/devs0n/observer/lecture/ChatServer.kt
+++ kt-gof/src/main/kotlin/com/tistory/devs0n/observer/lecture/ChatServer.kt
@@ -3,23 +3,23 @@ package com.tistory.devs0n.observer.lecture
 class ChatServer {
     private val subscribers: MutableMap<String, MutableList<Subscriber>> = mutableMapOf()
 
-    fun register(subject: String, subscriber: Subscriber) {
-        if (!this.subscribers.containsKey(subject)) {
-            this.subscribers[subject] = mutableListOf()
+    fun register(topic: String, subscriber: Subscriber) {
+        if (!this.subscribers.containsKey(topic)) {
+            this.subscribers[topic] = mutableListOf()
         }
 
-        this.subscribers[subject]!!.add(subscriber)
+        this.subscribers[topic]!!.add(subscriber)
     }
 
-    fun unregister(subject: String, subscriber: Subscriber) {
-        if (this.subscribers.containsKey(subject)) {
-            this.subscribers[subject]!!.remove(subscriber)
+    fun unregister(topic: String, subscriber: Subscriber) {
+        if (this.subscribers.containsKey(topic)) {
+            this.subscribers[topic]!!.remove(subscriber)
         }
     }
 
-    fun sendMessage(from: Subscriber, subject : String, message: String) {
-        if (this.subscribers.containsKey(subject)) {
-            val subscribers = this.subscribers[subject]!!
+    fun sendMessage(from: Subscriber, topic : String, message: String) {
+        if (this.subscribers.containsKey(topic)) {
+            val subscribers = this.subscribers[topic]!!
             subscribers.forEach {
                 it.onMessage("[${from}] $message")
             }
