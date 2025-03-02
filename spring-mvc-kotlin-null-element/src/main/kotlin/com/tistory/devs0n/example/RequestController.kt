4bc6fa510d58a85f465fb1dc07ff59ac0b833e7b
devson <sonyc5720@gmail.com>
2025-03-03 00:38:49 +0900

    Spring MVC Kotlin null element

--- spring-mvc-kotlin-null-element/src/main/kotlin/com/tistory/devs0n/example/RequestController.kt
+++ spring-mvc-kotlin-null-element/src/main/kotlin/com/tistory/devs0n/example/RequestController.kt
@@ -1,5 +1,7 @@
 package com.tistory.devs0n.example
 
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
 import kotlinx.serialization.Serializable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -7,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RequestController {
+    @PostMapping("/filter")
+    fun filter(@RequestBody request: FilterRequest): FilterRequest {
+        println("Contains null? : ${request.values.any { it == null }}")
+        return request.copy(
+            values = request.values.filter { it != null },
+        )
+    }
+
     @PostMapping("/jackson")
     fun jackson(@RequestBody request: JacksonRequest): JacksonRequest {
         println("Contains null? : ${request.values.any { it == null }}")
@@ -20,7 +30,12 @@ class RequestController {
     }
 }
 
+data class FilterRequest(
+    val values: List<String>,
+)
+
 data class JacksonRequest(
+    @JsonSetter(contentNulls = Nulls.SKIP)
     val values: List<String>,
 )
 
